5658fce0893d286a420c36969b1b820197af0c4c
dlongo <dlongodesign@gmail.com>
2022-03-10 00:36:42 -0500

    initial setup: sass, parcel, open-props with ligth and dark theme configured

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+// switch theme
+const switcher = document.querySelector('#theme-switcher')
+const doc = document.firstElementChild
+
+switcher.addEventListener('input', e =>
+    setTheme(e.target.value))
+
+const setTheme = theme =>
+    doc.setAttribute('color-scheme', theme)
