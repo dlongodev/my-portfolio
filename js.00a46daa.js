af252137427df87086104743baee94d097545e82
dlongo <dlongodesign@gmail.com>
2022-03-20 16:16:26 -0400

    fix nav menu bug

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -136,20 +136,18 @@ var setTheme = function setTheme(theme) {
 var hamburgerBtn = document.querySelector('#hamBtn');
 var toggleMenu = document.querySelector('#menuToggle');
 var ham = document.querySelector(".ham");
-hamburgerBtn.addEventListener('click', function (e) {
-  e.stopPropagation();
+hamburgerBtn.addEventListener('click', function () {
+  // e.stopPropagation()
   toggleMenu.classList.toggle('menu-active');
 }); // click outside menu
-
-window.addEventListener('click', function (e) {
-  e.stopPropagation();
-
-  if (!toggleMenu.contains(e.target) && hamburgerBtn) {
-    console.log('clicked outside');
-    toggleMenu.classList.toggle('menu-active');
-    ham.classList.toggle('active');
-  }
-});
+// window.addEventListener('click', (e) => {
+//     e.stopPropagation()
+//     if (!toggleMenu.contains(e.target) && hamburgerBtn) {
+//         console.log('clicked outside')
+//         toggleMenu.classList.toggle('menu-active')
+//         ham.classList.toggle('active')
+//     }
+// })
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -178,7 +176,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51100" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63701" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
