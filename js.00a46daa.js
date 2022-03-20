b5307a05b82d05cafcc9bd285bd2f7c64defd138
dlongo <dlongodesign@gmail.com>
2022-03-20 16:48:03 -0400

    mobile overflow hidden

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -176,7 +176,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63701" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54566" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
