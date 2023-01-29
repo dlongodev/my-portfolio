26fef6f3a9c82c9a08ecf527548dbd06f4dba973
Daniele Longo <daniele.longo@bidtellect.com>
2023-01-28 21:32:57 -0500

    :hammer: fix radio buttons for theme

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,8 @@
 const switcher = document.querySelector('#theme-switcher')
 const doc = document.firstElementChild
 
-switcher.addEventListener('input', e =>
-  setTheme(e.target.value))
+switcher.addEventListener('input', e =>{
+  setTheme(e.target.value)})
 
 const setTheme = theme =>
   doc.setAttribute('color-scheme', theme)
