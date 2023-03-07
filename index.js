eb81dfdc0280ac0c8a2729bc76998d62192262b1
Daniele Longo <daniele.longo@bidtellect.com>
2023-03-07 10:23:50 -0500

    Updates

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54669" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55397" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
